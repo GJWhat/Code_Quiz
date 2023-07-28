524b91fb5b58d55eb825bc4124bef773f8cc767c
GJWhat <Gavwatton@hotmail.com>
2023-07-28 13:17:34 +0100

    added someVars

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,1 +1,47 @@
-var window.quizData = quizData;
+import quizData from "./questions";
+// timer
+
+// var timerEl = document.getElementById('countdown');
+// var mainEl = document.getElementById('main');
+// var startBtn = document.getElementById('start');
+
+var startButton = document.getElementById("start");
+var questionContainerElement = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var answerButtonsElement = document.getElementById("answer-buttons");
+
+// Add an event listener to the "Start Quiz" button
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  startButton.style.display = "none";
+  questionContainer.classList.remove("hide");
+  loadQuestion();
+  startTimer();
+}
+function loadQuestion() {
+  if (currentQuestionIndex < quizData.length) {
+    var currentQuestion = quizData[currentQuestionIndex];
+
+    // Update the question text
+    questionElement.textContent = currentQuestion.question;
+
+    // Clear previous answer buttons
+    answerButtonsElement.innerHTML = "";
+
+    // Create buttons for each answer choice
+    currentQuestion.choices.forEach((choice) => {
+      const button = document.createElement("button");
+      button.textContent = choice;
+      button.classList.add("choice-btn");
+      button.addEventListener("click", () => handleAnswerClick(choice));
+      answerButtonsElement.appendChild(button);
+    });
+
+    // Show the question container
+    questionContainerElement.classList.remove("hide");
+  } else {
+    // No more questions, end the quiz
+    endQuiz();
+  }
+}
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,58 +1,49 @@
 //array of objects, each object is a question
 var quizData = [
-    {
-    question:
-        "Inside which HTML element do we put the JavaScript?",
-        choices:[
-            "<script>",
-            "<js>",
-            "<javascript>",
-            "<scripting>"],
-    
-        correctAnswer: "<script>",
-    },
-    {
-    question:
-        "Where is the correct place to insert a JavaScript?",
-        choices:[
-            "<script>",
-            "<js>",
-            "<javascript>",
-            "<scripting>"],
-    
-        correctAnswer: "<script>",
-      },
-    {
-    question:
-        "What is the correct JavaScript syntax to change the content of the HTML element below?",
-        choices: [
-            "#demo.innerHTML = "Hello World!";",
-            "document.getElementByName("p").innerHTML = "Hello World!";",
-            "document.getElement("p").innerHTML = "Hello World!";",
-            "document.getElementById("demo").innerHTML = "Hello World!";",
-        ],
-        correctAnswer: "document.getElementById("demo").innerHTML = "Hello World!";",
-    },
-    {
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<js>", "<javascript>", "<scripting>"],
+
+    correctAnswer: "<script>",
+  },
+  {
     question: "Where is the correct place to insert a JavaScript?",
-        choices: [
-          "Both the <head> section and the <body> section are correct",
-          "The <head> section",
-          "The <body> section;",
-          "The <footer> section"
-        ],
-        correctAnswer: "Both the <head> section and the <body> section are correct",
-      },
-        {
-    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choices: [
-            "<script href='xxx.js'>",
-            "<script name='xxx.js'>",
-            "<script src='xxx.js'>",
-            "<script file='xxx.js'>"
-        ],
-        correctAnswer: "<script src='xxx.js'>",
-    },
-    ];
-      // Export the quizData so that it can be imported in the main JavaScript file
-      window.quizData = quizData
+    choices: ["<script>", "<js>", "<javascript>", "<scripting>"],
+
+    correctAnswer: "<script>",
+  },
+  // {
+  // question:
+  //     "What is the correct JavaScript syntax to change the content of the HTML element below?",
+  //     choices: [
+  //         "#demo.innerHTML = 'Hello World!';",
+  //         "document.getElementByName("p").innerHTML = 'Hello World!';",
+  //         "document.getElement("p").innerHTML = 'Hello World!'",
+  //         "document.getElementById("demo").innerHTML = "Hello World!";",
+  //     ],
+  //     correctAnswer: "document.getElementById("demo").innerHTML = "Hello World!";",
+  // },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "Both the <head> section and the <body> section are correct",
+      "The <head> section",
+      "The <body> section;",
+      "The <footer> section",
+    ],
+    correctAnswer: "Both the <head> section and the <body> section are correct",
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "<script href='xxx.js'>",
+      "<script name='xxx.js'>",
+      "<script src='xxx.js'>",
+      "<script file='xxx.js'>",
+    ],
+    correctAnswer: "<script src='xxx.js'>",
+  },
+];
+// Export the quizData so that it can be imported in the main JavaScript file
+window.quizData = quizData;
